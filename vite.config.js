5c6332e4c4e7b1bfc46629814c3545cd9d153ca1
Jorge Victor <85069314+JVLJunior@users.noreply.github.com>
2025-06-05 20:38:02 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/audio-para-igrejas/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
